3ce118f424f7f7404df8b596a91ed4573f509fba
SotaCode <sotacodeprivate@gmail.com>
2022-02-15 15:23:28 -0300

    operadores aritmeticos added

---
+++ 01-introduccion/03-aritmeticos/aritmeticos.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 20
+	b := 10
+
+	//suma
+	result := a + b
+	fmt.Println(result)
+	//resta
+	result = a - b
+	fmt.Println(result)
+	//multiplicacion
+	result = a * b
+	fmt.Println(result)
+	//division
+	result = a / b
+	fmt.Println(result)
+
+}
