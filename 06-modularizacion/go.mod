68618047f872d170bcb512ba3f47c3e468776dfc
SotaCode <sotacodeprivate@gmail.com>
2022-02-17 19:20:58 -0300

    Modules upload github added

--- 06-modularizacion/go.mod
+++ 06-modularizacion/go.mod
@@ -1,3 +1,9 @@
 module paquetes
 
 go 1.17
+
+require (
+	github.com/donvito/hellomod v1.0.1 // indirect
+	github.com/sotacode/goModuleFiguras v0.0.0-20220217221337-64d27ba4d38a // indirect
+
+)
--- 06-modularizacion/main.go
+++ 06-modularizacion/main.go
@@ -1,17 +1,22 @@
 package main
 
-import (
-	"paquetes/figuras"
-)
+import "github.com/donvito/hellomod"
+
+//import "github.com/sotacode/goModuleFiguras"
 
 func main() {
 	/* fmt.Println()
 	mensajes.Saludar()
 	mensajes.Imprimir() */
 
-	cua1 := figuras.Cuadrado{Lado: 8.5}
+	/* cua1 := figuras.Cuadrado{Lado: 8.5}
 	figuras.Medidas(&cua1)
 
 	cir1 := figuras.Circulo{Radio: 2.54}
-	figuras.Medidas(&cir1)
+	figuras.Medidas(&cir1) */
+	hellomod.SayHello()
+
+	/* cu1 := goModuleFiguras.Circulo{Lado: 4.8}
+	goModuleFiguras.Medidas(cu1) */
+
 }
