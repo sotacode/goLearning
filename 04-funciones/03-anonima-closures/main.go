e16034ed4b8ba77130b53ba21d4683d99f786acc
SotaCode <sotacodeprivate@gmail.com>
2022-02-16 22:19:22 -0300

    clousures added

--- 04-funciones/03-anonima-closures/main.go
+++ 04-funciones/03-anonima-closures/main.go
@@ -1,18 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+//clousures
+
+func repeat(n int) func(cadena string) string {
+	return func(cadena string) string {
+		return strings.Repeat(cadena, n)
+	}
+}
 
 func main() {
 
+	repeat3 := repeat(3)
+
+	fmt.Println(repeat3("hola "))
+	fmt.Println(repeat3("Sota "))
 	//Funcion anonima se pueden asignar a variables
 	/* func() {
 		fmt.Println("Hola desde func anonima")
 	}() */
 
-	myfunc := func(nombre string) string {
+	/* myfunc := func(nombre string) string {
 		return fmt.Sprintf("Hola %s te saludo desde la func anonima", nombre)
 	}
 
 	mensaje := myfunc("Sota")
-	fmt.Println(mensaje)
+	fmt.Println(mensaje) */
 }
