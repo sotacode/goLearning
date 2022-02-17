6c8aee503f1f58ace0b936ca031c74a65b3f2db4
SotaCode <sotacodeprivate@gmail.com>
2022-02-17 13:07:29 -0300

    relation one to much added

--- 05-estructuras-interfaces/03-relaciones/main.go
+++ 05-estructuras-interfaces/03-relaciones/main.go
@@ -2,20 +2,31 @@ package main
 
 import "fmt"
 
-type User struct {
+//relaciones one to one
+/* type User struct {
 	name  string
 	email string
 	state bool
 }
-
 type Student struct {
 	user   User
 	codigo string
+} */
+
+//relaciones one to much
+type Curso struct {
+	titulo string
+	videos []Video
+}
+
+type Video struct {
+	titulo string
+	curso  Curso
 }
 
 func main() {
 
-	sota := User{"Sota", "holarina1@gmail.com", false}
+	/* sota := User{"Sota", "holarina1@gmail.com", false}
 
 	isi := User{"isi", "isi@gmail.com", true}
 
@@ -24,5 +35,22 @@ func main() {
 		codigo: "0xsd54w",
 	}
 	fmt.Println(isi)
-	fmt.Println(sotaStudent.user)
+	fmt.Println(sotaStudent.user) */
+
+	v1 := Video{titulo: "Intro"}
+	v2 := Video{titulo: "Medium course"}
+	v3 := Video{titulo: "finish course"}
+
+	curso := Curso{
+		titulo: "Curso de Go",
+		videos: []Video{v1, v2, v3},
+	}
+	v1.curso = curso
+	v2.curso = curso
+	v3.curso = curso
+
+	fmt.Println(v1.curso.titulo)
+	for _, video := range curso.videos {
+		fmt.Println(video.titulo)
+	}
 }
