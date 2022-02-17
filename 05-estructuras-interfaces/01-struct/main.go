5e3d05e89442b88d1a0ab6fc42e41f0e9b96b611
SotaCode <sotacodeprivate@gmail.com>
2022-02-17 12:32:10 -0300

    herencia added

--- 05-estructuras-interfaces/01-struct/main.go
+++ 05-estructuras-interfaces/01-struct/main.go
@@ -14,9 +14,24 @@ func (p *Persona) nuevoNombre(nuevoNombre string) {
 	p.nombre = nuevoNombre
 }
 
+//Herencia
+type Empleado struct {
+	Persona
+	sueldo float64
+}
+
 func main() {
 	p1 := Persona{"Nelsota", 24}
+	//p1.imprimir()
+	//p1.nuevoNombre("Sota")
 	p1.imprimir()
-	p1.nuevoNombre("Sota")
-	p1.imprimir()
+
+	e1 := Empleado{
+		sueldo: 500.0,
+	}
+	e1.nombre = "Pedro"
+	e1.edad = 30
+	e1.imprimir()
+	fmt.Println(e1)
+
 }
